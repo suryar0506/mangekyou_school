18fa05dbbb6354e2627aa0fd2b7eb316afcccc55
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-03-07 08:19:06 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB-H5dZLdq476rXWLf4ymgYAK4iY1VRFqg",
+    authDomain: "school-chat-baef4.firebaseapp.com",
+    databaseURL: "https://school-chat-baef4-default-rtdb.firebaseio.com",
+    projectId: "school-chat-baef4",
+    storageBucket: "school-chat-baef4.appspot.com",
+    messagingSenderId: "727326680460",
+    appId: "1:727326680460:web:6a0ceaf0fe52f0fb8062a9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
